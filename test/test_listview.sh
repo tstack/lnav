87333dd18629c51d12ba77ad7235c0c5e0f5dbaa
Timothy Stack <tstack@promb-1n-dhcp122.eng.vmware.com>
2009-10-15 08:58:45 -0700

    fix the listview tests

--- test/test_listview.sh
+++ test/test_listview.sh
@@ -5,32 +5,32 @@ run_test ./scripty -n -e ${srcdir}/listview_output.0 -- \
 
 on_error_fail_with "listview output does not match?"
 
-run_test ./scripty -- ./drive_listview -t 1 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.1 -- \
+    ./drive_listview -t 1 < /dev/null
 
-check_output "Listview didn't move down?" < \
-    ${srcdir}/listview_output.1
+on_error_fail_with "listview didn't move down?"
 
-run_test ./scripty -- ./drive_listview -l 1 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.2 -- \
+    ./drive_listview -l 1 < /dev/null
 
-check_output "Listview didn't move right?" < \
-    ${srcdir}/listview_output.2
+on_error_fail_with "Listview didn't move right?"
 
-run_test ./scripty -- ./drive_listview -t 1 -l 1 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.3 -- \
+    ./drive_listview -t 1 -l 1 < /dev/null
 
-check_output "Listview didn't move left and right?" < \
-    ${srcdir}/listview_output.3
+on_error_fail_with "Listview didn't move left and right?"
 
-run_test ./scripty -- ./drive_listview -y 1 -r 50 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.4 -- \
+    ./drive_listview -y 1 -r 50 < /dev/null
 
-check_output "Listview doesn't start down one line?" < \
-    ${srcdir}/listview_output.4
+on_error_fail_with "Listview doesn't start down one line?"
 
-run_test ./scripty -- ./drive_listview -y 1 -r 50 -h -1 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.5 -- \
+    ./drive_listview -y 1 -r 50 -h -1 < /dev/null
 
-check_output "Listview isn't shorter?" < \
-    ${srcdir}/listview_output.5
+on_error_fail_with "Listview isn't shorter?"
 
-run_test ./scripty -- ./drive_listview -y 1 -r 50 -h -1 -t 1 < /dev/null
+run_test ./scripty -n -e ${srcdir}/listview_output.6 -- \
+    ./drive_listview -y 1 -r 50 -h -1 -t 1 < /dev/null
 
-check_output "Listview didn't move down (2)?" < \
-    ${srcdir}/listview_output.6
+on_error_fail_with "Listview didn't move down (2)?"
