858abe79b6d32d464577e724f0037ae86b40aada
Timothy Stack <timothyshanestack@gmail.com>
2013-05-31 21:20:58 -0700

    [test] fix test_auto_mem test

--- test/test_auto_mem.cc
+++ test/test_auto_mem.cc
@@ -58,17 +58,17 @@ int main(int argc, char *argv[])
     struct my_data md1_val, md2_val;
     
     md1 = &md1_val;
-    assert(free_count == 1);
+    assert(free_count == 0);
     md1 = md2;
-    assert(free_count == 2);
+    assert(free_count == 1);
     assert(last_free == &md1_val);
     assert(md1 == NULL);
 
     md1 = &md2_val;
-    assert(free_count == 3);
-    assert(last_free == NULL);
+    assert(free_count == 1);
+    assert(last_free == &md1_val);
     *md1.out() = &md1_val;
-    assert(free_count == 4);
+    assert(free_count == 2);
     assert(last_free == &md2_val);
     assert(md1.in() == &md1_val);
 
@@ -76,11 +76,11 @@ int main(int argc, char *argv[])
 	auto_mem<struct my_data, my_free> md_cp(md1);
 
 	assert(md1 == NULL);
-	assert(free_count == 4);
+	assert(free_count == 2);
 	assert(md_cp == &md1_val);
     }
 
-    assert(free_count == 5);
+    assert(free_count == 3);
     assert(last_free == &md1_val);
     
     return retval;
