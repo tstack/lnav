eb1747d73bc5789db8f773b34ad3ec04b5c5d200
Timothy Stack <timothyshanestack@gmail.com>
2014-03-29 22:08:01 -0700

    build fix

--- src/init_sql.py
+++ src/init_sql.py
@@ -5,13 +5,15 @@ import httplib
 TABLES = """
 -- This file was created by init_sql.py --
 
-CREATE TABLE IF NOT EXISTS http_status_code (
+CREATE TABLE IF NOT EXISTS http_status_codes (
     status integer PRIMARY KEY,
-    message text
+    message text,
+
+    FOREIGN KEY(status) REFERENCES access_log(sc_status)
 );
 """
 
 print TABLES
 
 for item in httplib.responses.iteritems():
-    print "INSERT INTO http_status_code VALUES (%d, \"%s\");" % item
+    print "INSERT INTO http_status_codes VALUES (%d, \"%s\");" % item
