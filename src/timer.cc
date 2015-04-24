fcb2db38b03c9dd3e9d702e3c7557e23d592f955
Suresh Sundriyal <sureshsundriyal@gmail.com>
2015-04-24 01:37:58 -0700

    [timer] Start off disarmed + refactoring.

--- src/timer.cc
+++ src/timer.cc
@@ -1,3 +1,32 @@
+/**
+ * Copyright (c) 2015, Suresh Sundriyal
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "timer.hh"
 #include "lnav_log.hh"
 
@@ -11,7 +40,7 @@ timer::error::error(int err):e_err(err) { }
 timer::interrupt_timer::interrupt_timer(struct timeval t,
         sighandler_t_ sighandler=SIG_IGN) : new_handler(sighandler),
         old_handler(NULL), new_val((struct itimerval){{0,0},t}),
-        old_val(disable), armed(true) { }
+        old_val(disable), armed(false) { }
 
 int timer::interrupt_timer::arm_timer() {
     // Disable the interval timer before setting the handler and arming the
@@ -47,6 +76,10 @@ int timer::interrupt_timer::arm_timer() {
     return 0;
 }
 
+bool timer::interrupt_timer::is_armed() {
+    return this->armed;
+}
+
 void timer::interrupt_timer::disarm_timer() {
     if (this->armed) {
         // Disable the interval timer before resetting the handler and rearming
@@ -68,6 +101,9 @@ void timer::interrupt_timer::disarm_timer() {
                       strerror(errno));
             throw timer::error(errno);
         }
+        this->armed = false;
+        this->old_val = disable;
+        this->old_handler = NULL;
     }
 }
 
