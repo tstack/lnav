1ad3966a95c72d3e492ac80ccac15956a7a05afe
Timothy Stack <timothyshanestack@gmail.com>
2015-08-12 22:34:41 -0700

    [tests] add a couple more tests for the loader

--- test/test_format_loader.sh
+++ test/test_format_loader.sh
@@ -8,10 +8,12 @@ run_test ${lnav_test} -C \
 
 check_error_output "invalid format not detected?" <<EOF
 error:bad_regex_log.regex[std]:missing )
+error:bad_regex_log.level:missing )
 error:bad_regex_log:invalid sample -- 1428634687123; foo
 error:bad_sample_log:invalid sample -- 1428634687123; foo bar
 error:bad_sample_log:partial sample matched -- 1428634687123; foo
 error:  against pattern -- ^(?<timestamp>\d+); (?<body>\w+)$
 error:bad_sample_log:partial sample matched -- 1428634687123
 error:  against pattern -- ^(?<timestamp>\d+): (?<body>.*)$
+error:no_sample_log:no sample logs provided, all formats must have samples
 EOF
