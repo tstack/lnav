a3b417c2d50b3e250fda5c9a6682d1613c550136
Timothy Stack <timothyshanestack@gmail.com>
2015-08-19 21:15:34 -0700

    fix timestamps in test

--- test/test_cli.sh
+++ test/test_cli.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+export TZ="UTC"
 run_test ${lnav_test} -t -n <<EOF
 Hello, World!
 Goodbye, World!
 EOF
 
 check_output "file URL is not working" <<EOF
-2013-06-06T12:13:20.123  Hello, World!
-2013-06-06T12:13:20.123  Goodbye, World!
-2013-06-06T12:13:20.123  ---- END-OF-STDIN ----
+2013-06-06T19:13:20.123  Hello, World!
+2013-06-06T19:13:20.123  Goodbye, World!
+2013-06-06T19:13:20.123  ---- END-OF-STDIN ----
 EOF
