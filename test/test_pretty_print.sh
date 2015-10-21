7a476ca42d1a22a47cf172be0db86378dc6960f9
Timothy Stack <timothyshanestack@gmail.com>
2015-10-21 07:12:50 -0700

    use a more reliable ip address for test

--- test/test_pretty_print.sh
+++ test/test_pretty_print.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-echo '2015-04-18T13:16:30.003 198.51.100.45 <foo>98.51.100.154</foo>9 198.158.100.4<1054 198.51.100.1546 544.9.8.7 98.542.241.99 19143.2.5.6' | \
+echo '2015-04-18T13:16:30.003 8.8.8.8 <foo>8.8.8.8</foo>9 8.8.8.8<1054 198.51.100.1546 544.9.8.7 98.542.241.99 19143.2.5.6' | \
     run_test ${lnav_test} -n -c ":switch-to-view pretty"
 
 check_output "pretty print not able to properly grok ipv4?" <<EOF
-2015-04-18T13:16:30.003 198.51.100.45 (unknown)
-<foo>98.51.100.154 (unknown)</foo>
-9 198.158.100.4 (unknown)<1054 198.51.100.1546 544.9.8.7 98.542.241.99 19143.2.5.6
+2015-04-18T13:16:30.003 8.8.8.8 (google-public-dns-a.google.com)
+<foo>8.8.8.8 (google-public-dns-a.google.com)</foo>
+9 8.8.8.8 (google-public-dns-a.google.com)<1054 198.51.100.1546 544.9.8.7 98.542.241.99 19143.2.5.6
 EOF
