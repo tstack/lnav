6821bae882a035f7c5f39d7c8d7cf706e2f57d19
Timothy Stack <timothyshanestack@gmail.com>
2015-12-18 22:54:32 -0800

    fix one part of the build break

--- test/test_shlexer.sh
+++ test/test_shlexer.sh
@@ -3,7 +3,7 @@
 export FOO='bar'
 export DEF='xyz'
 
-run_test drive_shlexer '$FOO'
+run_test ./drive_shlexer '$FOO'
 
 check_output "var ref" <<EOF
     \$FOO
@@ -11,7 +11,7 @@ ref ^--^
 eval -- bar
 EOF
 
-run_test drive_shlexer '\a'
+run_test ./drive_shlexer '\a'
 
 check_output "escape" <<EOF
     \a
@@ -19,7 +19,7 @@ esc ^^
 eval -- a
 EOF
 
-run_test drive_shlexer "'abc'"
+run_test ./drive_shlexer "'abc'"
 
 check_output "single" <<EOF
     'abc'
@@ -28,7 +28,7 @@ sen     ^
 eval -- abc
 EOF
 
-run_test drive_shlexer '"def"'
+run_test ./drive_shlexer '"def"'
 
 check_output "double" <<EOF
     "def"
@@ -37,7 +37,7 @@ den     ^
 eval -- def
 EOF
 
-run_test drive_shlexer '"abc $DEF 123"'
+run_test ./drive_shlexer '"abc $DEF 123"'
 
 check_output "double w/ref" <<EOF
     "abc \$DEF 123"
@@ -47,7 +47,7 @@ den              ^
 eval -- abc xyz 123
 EOF
 
-run_test drive_shlexer "'abc \$DEF 123'"
+run_test ./drive_shlexer "'abc \$DEF 123'"
 
 check_output "single w/ref" <<EOF
     'abc \$DEF 123'
@@ -56,7 +56,7 @@ sen              ^
 eval -- abc \$DEF 123
 EOF
 
-run_test drive_shlexer 'abc $DEF 123'
+run_test ./drive_shlexer 'abc $DEF 123'
 
 check_output "unquoted" <<EOF
     abc \$DEF 123
