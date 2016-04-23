151b0493d089a9b32eefb7790c0794cf52916494
Suresh Sundriyal <sureshsundriyal@gmail.com>
2016-04-22 19:48:17 -0700

    [commands] Read and honor the 'LNAVSECURE' environment variable.
    Read the value of the 'LNAVSECURE' environment variable upfront and
    store it in the lnav_data structure. When this variable is set prior to
    the binary execution, the following commands are disabled:
    
    * 'open'
    * 'pipe-tp'
    * 'pipe-line-to'
    * 'save-*-to'
    
    This is a proposed fix for tstack/lnav#305.

--- src/lnav_commands.cc
+++ src/lnav_commands.cc
@@ -454,6 +454,10 @@ static string com_save_to(string cmdline, vector<string> &args)
         return "";
     }
 
+    if (lnav_data.ld_secure_mode) {
+        return args[0] + ": unavailable in secure mode";
+    }
+
     if (args.size() < 2) {
         return "error: expecting file name or '-' to write to the terminal";
     }
@@ -646,6 +650,10 @@ static string com_pipe_to(string cmdline, vector<string> &args)
     if (args.size() == 0) {
         args.push_back("filename");
         return "";
+    }
+
+    if (lnav_data.ld_secure_mode) {
+        return args[0] + ": unavailable in secure mode";
     }
 
     if (args.size() < 2) {
@@ -1341,6 +1349,9 @@ static string com_open(string cmdline, vector<string> &args)
     if (args.size() == 0) {
         args.push_back("filename");
         return "";
+    }
+    else if (lnav_data.ld_secure_mode) {
+        return args[0] + ": unavailable in secure mode";
     }
     else if (args.size() < 2) {
         return retval;
