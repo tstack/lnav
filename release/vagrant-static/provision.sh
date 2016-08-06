4d091988a09a1738d9adb54e9e33ef4a41e347d0
Timothy Stack <timothyshanestack@gmail.com>
2016-08-06 15:23:23 -0700

    [release] some more work on the release build

--- release/vagrant-static/provision.sh
+++ release/vagrant-static/provision.sh
@@ -9,21 +9,6 @@ export PATH=${FAKE_ROOT}/bin:${PATH}
 
 cd ~/github
 
-PACKAGE_URLS="\
-    http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz \
-    http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz \
-    https://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz \
-    ftp://invisible-island.net/ncurses/ncurses-5.9.tar.gz \
-    ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz \
-    ftp://ftp.cwru.edu/pub/bash/readline-6.3.tar.gz \
-    http://zlib.net/zlib-1.2.8.tar.gz \
-    http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz \
-    http://sqlite.org/2016/sqlite-autoconf-3130000.tar.gz \
-    http://openssl.skazkaforyou.com/source/openssl-1.0.1t.tar.gz \
-    http://www.libssh2.org/download/libssh2-1.7.0.tar.gz \
-    http://curl.haxx.se/download/curl-7.48.0.tar.gz \
-    "
-
 SQLITE_CFLAGS="\
     -DSQLITE_ENABLE_COLUMN_METADATA \
     -DSQLITE_SOUNDEX \
@@ -32,15 +17,10 @@ SQLITE_CFLAGS="\
     -DSQLITE_ENABLE_JSON1 \
     "
 
-cd ~/packages
-
-for url in $PACKAGE_URLS; do
-    wget --no-check-certificate -N $url
-done
 
 cd ~/extract
 
-for pkg in ~/packages/*.tar.gz; do
+for pkg in /vagrant/pkgs/*.tar.gz; do
     tar xfz $pkg
 done
 
