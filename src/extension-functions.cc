143b35603192599ff336cc3c28598a4ffb56cb0e
Timothy Stack <timothyshanestack@gmail.com>
2017-04-19 14:36:10 -0700

    Fixes #441

--- src/extension-functions.cc
+++ src/extension-functions.cc
@@ -269,7 +269,7 @@ static const int xtra_utf8_bits[] =  {
 ** masking the character with utf8_mask[N] must produce a non-zero
 ** result.  Otherwise, we have an (illegal) overlong encoding.
 */
-static const long utf_mask[] = {
+static const unsigned long utf_mask[] = {
   0x00000000,
   0xffffff80,
   0xfffff800,
