265f80c60d7fc17263403bd43b97b3a509415e04
Timothy Stack <timothyshanestack@gmail.com>
2018-08-22 06:45:20 -0700

    [test] locale tweak

--- test/test_logfile.sh
+++ test/test_logfile.sh
@@ -2,12 +2,9 @@
 
 locale -a
 env
-if ! test -e x"$LOCPATH"; then
-  echo "no loc!?"
-fi
 cp ${srcdir}/logfile_syslog_fr.0 logfile_syslog_fr.0
 touch -t 200711030923 logfile_syslog_fr.0
-run_test env LC_TIME=fr_FR.UTF-8 ${lnav_test} -n \
+run_test env LC_ALL=fr_FR.UTF-8 ${lnav_test} -n \
     -c ";SELECT log_time FROM syslog_log" \
     -c ":write-csv-to -" \
     logfile_syslog_fr.0
