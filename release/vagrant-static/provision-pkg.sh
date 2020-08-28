b40874069790b69e30a535806f9be5265b4ca350
Timothy Stack <timothyshanestack@gmail.com>
2020-08-27 21:59:40 -0700

    [release] fix some issues in the release process

--- release/vagrant-static/provision-pkg.sh
+++ release/vagrant-static/provision-pkg.sh
@@ -3,5 +3,5 @@
 # Installs dependencies for running fpm
 
 sudo apt-get update
-sudo apt-get install -y ruby-dev gcc make zip unzip rpm
+sudo apt-get install -y ruby2.3 ruby-dev gcc make zip unzip rpm
 sudo gem install fpm
