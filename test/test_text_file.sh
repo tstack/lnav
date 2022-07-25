a459370bd80b845e480c4f2049cfecf3fc71702b
Timothy Stack <timothyshanestack@gmail.com>
2022-07-25 07:49:51 -0700

    [tests] coverage for markdown breadcrumbs

--- test/test_text_file.sh
+++ test/test_text_file.sh
@@ -7,4 +7,10 @@ run_cap_test ${lnav_test} -n \
     ${top_srcdir}/README.md
 
 run_cap_test ${lnav_test} -n \
+    -c ':goto 115' \
+    -c ";SELECT top_meta FROM lnav_views WHERE name = 'text'" \
+    -c ':write-json-to -' \
+    ${top_srcdir}/README.md
+
+run_cap_test ${lnav_test} -n \
     ${top_srcdir}/src/log_level.cc
