fa0050be846cc07b763ec6d9203861f5ecda6651
Tim Stack <timothyshanestack@gmail.com>
2022-08-29 09:37:42 -0700

    [demo] set a timeout of 5 minutes for the first tutorial

--- docs/tutorials/tutorial1/run.sh
+++ docs/tutorials/tutorial1/run.sh
@@ -3,4 +3,4 @@
 export LNAVSECURE=1
 export TERM=xterm-256color
 
-lnav -I /tutorials/tutorial-lib /tutorials/tutorial1/tutorial1.glog /tutorials/tutorial1/index.md#tutorial-1
+timeout --foreground 5m lnav -I /tutorials/tutorial-lib /tutorials/tutorial1/tutorial1.glog /tutorials/tutorial1/index.md#tutorial-1
