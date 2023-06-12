a403788d90113204089ae21e01f8cee9bb2aadc2
Tim Stack <timothyshanestack@gmail.com>
2023-06-11 22:22:01 -0700

    [build] fix some typos

--- .github/actions/muslbuilder/entrypoint.sh
+++ .github/actions/muslbuilder/entrypoint.sh
@@ -3,6 +3,16 @@
 set -Eeuxo pipefail
 
 cd $GITHUB_WORKSPACE
-./autogen.sh
-./configure
+mkdir lbuild
+cd lbuild
+../autogen.sh
+../configure \
+    --with-libarchive=/fake.root \
+    CFLAGS='-static -g1 -gz=zlib -no-pie -O2' \
+    CXXFLAGS='-static -g1 -gz=zlib -U__unused -no-pie -O2' \
+    LDFLAGS="-L/fake.root/lib" \
+    CPPFLAGS="-I/fake.root/include" \
+    LIBS="-L/fake.root/lib -lexecinfo -lssh2 -llzma -lssl -lcrypto -lz" \
+    --enable-static \
+    PATH="/fake.root/bin:${PATH}"
 make -j4
