e04e3f57538a30cd1751812f2f721bb9b2338194
Tim Stack <timothyshanestack@gmail.com>
2023-07-02 09:07:01 -0700

    [build] fix rpm version for rc

--- release/makespec.sh
+++ release/makespec.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-DEFAULT_VERSION=$(head -1 ../configure.ac | cut -d, -f2)
+DEFAULT_VERSION=$(grep AC_INIT ../configure.ac | cut -d, -f2)
 DEFAULT_VERSION=${DEFAULT_VERSION#[}
 DEFAULT_VERSION=${DEFAULT_VERSION%]}
 
 if [ "$GITHUB_REF_TYPE" == 'tag' ]; then
-    export LNAV_VERSION_NUMBER=${GITHUB_REF#refs/tags/v}
+    export LNAV_VERSION_NUMBER=$(echo ${GITHUB_REF#refs/tags/v} | sed -e 's/-/~/g')
 else
     export LNAV_VERSION_NUMBER="${DEFAULT_VERSION}^"$(date +%Y%m%d).git$(git rev-parse --short HEAD)
 fi
