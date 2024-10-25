48e97f409b5418fd8a95eac79d650211c0391f83
Timothy Stack <tim@stack-email.org>
2024-10-24 21:07:13 -0700

    [tests] fix some SQL

--- test/formats/sqldir/init.sql
+++ test/formats/sqldir/init.sql
@@ -2,4 +2,4 @@
 CREATE VIEW web_status AS
   SELECT group_concat(cs_uri_stem), sc_status FROM access_log group by sc_status;
 
-INSERT into lnav_view_filters VALUES ("log", 5, 0, "in", "regex", "credential status");
+INSERT into lnav_view_filters VALUES ('log', 5, 0, 'in', 'regex', 'credential status');
