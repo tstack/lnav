ba2c50471d50c05a83c4151167eddbe7b698fc8c
Tim Stack <timothyshanestack@gmail.com>
2025-01-30 21:36:41 -0800

    [build] try to fix snap build

--- src/base/lnav.console.hh
+++ src/base/lnav.console.hh
@@ -36,8 +36,7 @@
 #include "base/attr_line.hh"
 #include "base/file_range.hh"
 
-namespace lnav {
-namespace console {
+namespace lnav::console {
 
 void println(FILE* file, const attr_line_t& al);
 
@@ -96,7 +95,7 @@ struct user_message {
 
     user_message() = default;
     user_message(user_message&&) = default;
-    user_message(const user_message&) noexcept = default;
+    user_message(const user_message&) = default;
 
     user_message& operator=(user_message&&) = default;
     user_message& operator=(const user_message&) = default;
@@ -184,7 +183,6 @@ struct user_message {
 
 void print(FILE* file, const user_message& um);
 
-}  // namespace console
-}  // namespace lnav
+}  // namespace lnav::console
 
 #endif
