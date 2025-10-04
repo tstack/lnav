5959521565769125de5f216afc57d252d7f98123
Tim Stack <timothyshanestack@gmail.com>
2025-10-04 13:36:36 -0700

    [tests] test source_log_stmt()

--- test/test_log2src.sh
+++ test/test_log2src.sh
@@ -9,3 +9,8 @@ run_cap_test ${lnav_test} -n \
     -c ";SELECT log_line, log_msg_src, log_msg_format, log_msg_values FROM all_logs" \
     -c ":write-jsonlines-to -" \
     ${test_dir}/log2src/python/python-example.0
+
+run_cap_test ${lnav_test} -nN \
+    -c ":add-source-path ${test_dir}/log2src/python" \
+    -c ";SELECT * FROM source_log_stmt('log_example.py')" \
+    -c ":write-jsonlines-to -"
