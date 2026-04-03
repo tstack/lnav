4d1ad7a54bfc9314d962447c66d3961c2091d90f
Timothy Stack <tim@stack-email.org>
2026-04-03 14:43:51 -0700

    [test] test piper with binary data

--- test/test_cli.sh
+++ test/test_cli.sh
@@ -55,6 +55,8 @@ rm -rf ./piper-tmp
 mkdir piper-tmp
 run_cap_test ${lnav_test} -n -e 'cat ${test_dir}/textfile_plain.0'
 
+run_cap_test ${lnav_test} -n -e 'cat ${test_dir}/textfile_broken_gz.txt.gz'
+
 run_cap_test ${lnav_test} -m piper list
 
 PIPER_URL=$(env NO_COLOR=1 ${lnav_test} -m -q piper list | tail -1 | sed -r -e 's;.*(piper://[^ ]+).*;\1;g')
